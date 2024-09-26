2d17ba36e974049faeeacb60c15273b334e75486
gabrielszg <gabrielsouza5@hotmail.com>
2024-09-26 15:17:58 -0300

    refactor: data brought from a game information list

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,36 @@
+export type CardData = {
+	cover: string;
+	label: string;
+	type: string;
+	price: string;
+};
+
+export const dataFake: CardData[] = [
+	{
+		cover: '/bt-1.jpg',
+		label: 'DIGITAL',
+		type: 'DIGITAL | PS4',
+		price: 'R$ 129,99',
+	},
+
+	{
+		cover: '/bt-4.jpg',
+		label: 'EXCLUSIVE',
+		type: 'DISC | PS5',
+		price: 'R$ 269,99',
+	},
+
+	{
+		cover: '/ac-cover.jpg',
+		label: 'BEST OF YEAR',
+		type: 'DIGITAL | PS5',
+		price: 'R$ 369,99',
+	},
+
+	{
+		cover: '/bt-hardline.jpg',
+		label: 'NEW',
+		type: 'DIGITAL | PS3 PS4 PS5',
+		price: 'R$ 369,99',
+	},
+];
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CardComponent } from "../../components/card/card.component";
+import { CardData, dataFake } from '../../data/dataFake';
 
 @Component({
   selector: 'app-home',
@@ -9,5 +10,5 @@ import { CardComponent } from "../../components/card/card.component";
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  gameList: CardData[] = dataFake;
 }
